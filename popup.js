32fdaf719964848c72bde2f04a099f2aa2c56388
Erik Post <erik@shinsetsu.nl>
2020-08-24 17:51:50 +0200

    Add org-mode list exporter.

--- popup.js
+++ popup.js
@@ -18,18 +18,19 @@ function main() {
   // dump('tab dump')
 
   var checkbox1 = document.getElementById('checkbox-1')
-  var button1 = document.getElementById('button-1')
+  var buttonDumpTabsAsOrgSections = document.getElementById('button-dump-tabs-as-org-sections')
+  var buttonDumpTabsAsOrgLists = document.getElementById('button-dump-tabs-as-org-lists')
 
   // update UI from (initial) state
   checkbox1.checked = state.dumpTabsFromAllWindows
 
   function onCheckChange(e) {
     // debug('e = '+ JSON.stringify(e))
-    debug('state pre  = '+ JSON.stringify(state))
+    //debug('state pre  = '+ JSON.stringify(state))
     var checkboxState = checkbox1.checked
-    debug('cb state   = '+ JSON.stringify(checkboxState))
+    //debug('cb state   = '+ JSON.stringify(checkboxState))
     state.dumpTabsFromAllWindows = checkboxState
-    debug('state post = '+ JSON.stringify(state))
+    //debug('state post = '+ JSON.stringify(state))
   }
 
   function _formatDictAsOrgSections(formatTab) {
@@ -43,8 +44,6 @@ function main() {
       }
 
       return "" +
-        '* bookmarks dumped\n' +
-        '\n' +
         R.compose(
           R.join('\n'),
           R.values,
@@ -55,6 +54,7 @@ function main() {
   }
 
   var formatDictAsOrgSections = _formatDictAsOrgSections(tab => '*** [[' + tab.url + '][' + tab.title + ']]')
+  var formatDictAsOrgList     = _formatDictAsOrgSections(tab => '- [[' + tab.url + '][' + tab.title + ']]')
 
   function formatDictAsJson(dict) {
     return "" +
@@ -67,31 +67,37 @@ function main() {
       '\n}';
   }
 
-  function dumpTabs() {
-    debug('state = '+ JSON.stringify(state))
-    // https://developer.chrome.com/extensions/tabs#method-query
-    chrome.tabs.query(
-      {
-        // active: true, // show only active tab
-        currentWindow: !state.dumpTabsFromAllWindows
-      },
-      function(tabs) {
-        // when the popup is opened, there is certainly a window and at least
-        // one tab (and at most one active tab), so tabs is a non-empty array
 
-        // R.map(t => dump(JSON.stringify(t)), tabs)
-        // var tabsDump = R.groupBy(t => t.windowId)(tabs)
-        // var tabsDump = R.compose(R.groupBy(t => t.windowId))(tabs)
+  function dumpTabs(formatDict) {
+    return function () {
+      // debug('state = '+ JSON.stringify(state))
+      // https://developer.chrome.com/extensions/tabs#method-query
+      chrome.tabs.query(
+        {
+          // active: true, // show only active tab
+          currentWindow: !state.dumpTabsFromAllWindows
+        },
+        function(tabs) {
+          // when the popup is opened, there is certainly a window and at least
+          // one tab (and at most one active tab), so tabs is a non-empty array
 
-        // var tabs = R.compose(formatDictAsJson, R.groupBy(t => t.windowId))(tabs)
-        var tabsDump = R.compose(formatDictAsOrgSections, R.groupBy(t => t.windowId))(tabs)
+          // R.map(t => dump(JSON.stringify(t)), tabs)
+          // var tabsDump = R.groupBy(t => t.windowId)(tabs)
+          // var tabsDump = R.compose(R.groupBy(t => t.windowId))(tabs)
 
-        // var tabsDump = R.compose(R.map(tabs => JSON.stringify(tabs)), R.groupBy(t => t.windowId))(tabs)
-        // dump(JSON.stringify(tabs, null, 2))
-        dump(tabsDump)
-      }
-    )
+          // var tabs = R.compose(formatDictAsJson, R.groupBy(t => t.windowId))(tabs)
+          var tabsDump = R.compose(formatDict, R.groupBy(t => t.windowId))(tabs)
+
+          // var tabsDump = R.compose(R.map(tabs => JSON.stringify(tabs)), R.groupBy(t => t.windowId))(tabs)
+          // dump(JSON.stringify(tabs, null, 2))
+          dump(tabsDump)
+        }
+      )
+    }
   }
+
+  var dumpTabsAsOrgSections = dumpTabs(formatDictAsOrgSections)
+  var dumpTabsAsOrgLists = dumpTabs(formatDictAsOrgList)
 
   // something like this can be used to restore tabs
   function openWindowWithTabs() {
@@ -110,6 +116,7 @@ function main() {
   }
 
   checkbox1.addEventListener('change', onCheckChange, false);
-  button1.addEventListener('click', dumpTabs, false);
+  buttonDumpTabsAsOrgSections.addEventListener('click', dumpTabsAsOrgSections, false);
+  buttonDumpTabsAsOrgLists.addEventListener('click', dumpTabsAsOrgLists, false);
 }
 document.addEventListener('DOMContentLoaded', main)
